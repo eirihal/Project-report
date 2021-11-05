41144a223e2c32475d428b13f27b4acc3f6581d7
ulrikeriks <92155799+ulrikeriks@users.noreply.github.com>
2021-11-05 10:19:23 +0100

    Update TPK4161 Markov.py

--- TPK4161 Markov.py
+++ TPK4161 Markov.py
@@ -1,13 +1,15 @@
 import numpy as np
 
-FR = 0.005 # failure rate
-RR = 0.125 # repair rate
+FR = 0.005/60 # failure rate per minute
+RR = 0.125/60 # repair rate per minute
 
 A = np.array([[-FR, FR], [RR, -RR]]) # Transition rate matrix
 
 I = np.array([[1, 0], [0, 1]]) # Identity matrix
 
 P = np.array([1, 0]) # P(0), P is updated through iteration
+
+Result = []
 
 hours_per_day = 8
 minutes_per_day = 8*60
@@ -17,5 +19,7 @@ for i in range(1, minutes_per_day + 1):
     A_d = A * delta
     A_I = A_d + I
     P = np.matmul(P, A_I)
+    Result.append(P[1])
 
-print(P)    
+Result = np.mean(Result)
+print(Result)
